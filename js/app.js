f516db69a67013a1bdfbff95a3c72999970d271a
feer.rivero <fer_eze_rivero@hotmail.com>
2023-04-02 18:39:42 -0300

    agregando script a tiendaFer

---
+++ js/app.js
@@ -0,0 +1,16 @@
+alert ("¡Bienvenido a tiendaFer, el lugar donde encontrarás las mejores ofertas!");
+
+let preguntaNovedades = confirm ("¿Te gustaria recibir las ultimas novedades y las mejores ofertas?");
+
+if (preguntaNovedades == true){
+    
+    let correo = prompt ("Ingrese su correo electronico.");
+
+    if ((correo != null) && (correo != "")) {
+        alert ("¡Te enteraras de nuestras novedades antes que nadie!");}
+
+    else { alert ("No hay problema. Siempre podrás encontrar las mejores ofertas en nuestra web.")};
+
+else { alert ("No hay problema. Siempre podrás encontrar las mejores ofertas en nuestra web.")}; 
+
+};
